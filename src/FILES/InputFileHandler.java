809c6704e635cc474578f898c55aed747f503aba
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-03-26 19:51:01 +0800

    added InputFileHandler attributes and methods

--- src/FILES/InputFileHandler.java
+++ src/FILES/InputFileHandler.java
@@ -1,7 +1,32 @@
 package FILES;
 
+import java.util.ArrayList;
+
 /**
  * Created by alvinjay on 3/26/15.
  */
 public class InputFileHandler {
+    private ArrayList<String> inputValues;
+
+    public InputFileHandler() {
+        this.inputValues = new ArrayList<String>();
+    }
+
+    /**
+     * Checks whether a DFA file line is valid
+     * @param inputLine
+     * @return
+     */
+    public static boolean isInputLineValid(String inputLine) {
+        //TODO check if input line is valid
+        return true;
+    }
+
+    /* GETTER METHODS */
+
+    public ArrayList<String> getInputValues() {
+        return inputValues;
+    }
+
+    /* END GETTER METHODS */
 }
