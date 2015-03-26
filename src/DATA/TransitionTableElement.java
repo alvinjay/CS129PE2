ff42a623b22e38b662b3a218df1c7d205ec5cc28
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-03-26 19:31:37 +0800

    added TransitionTableElement class attributes and methods

--- src/DATA/TransitionTableElement.java
+++ src/DATA/TransitionTableElement.java
@@ -4,5 +4,28 @@ package DATA;
  * Created by alvinjay on 3/26/15.
  */
 public class TransitionTableElement {
+    private String type, label, zeroTransition, oneTransition;
 
+    public TransitionTableElement(String type, String label, String zeroTransition, String oneTransition) {
+        this.type = type;
+        this.label = label;
+        this.zeroTransition = zeroTransition;
+        this.oneTransition = oneTransition;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getZeroTransition() {
+        return zeroTransition;
+    }
+
+    public String getOneTransition() {
+        return oneTransition;
+    }
 }
