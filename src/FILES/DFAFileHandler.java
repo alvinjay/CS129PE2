fc0b885146c954ef04e22383a8a1a90081c7fad4
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-03-26 19:46:39 +0800

    added DFAFileHandler methods

--- src/FILES/DFAFileHandler.java
+++ src/FILES/DFAFileHandler.java
@@ -1,7 +1,34 @@
 package FILES;
 
+import DATA.TransitionTableElement;
+
+import java.util.ArrayList;
+
 /**
  * Created by alvinjay on 3/26/15.
  */
 public class DFAFileHandler {
+    private ArrayList<TransitionTableElement> transitionTableValues;
+
+    public DFAFileHandler(ArrayList<TransitionTableElement> transitionTableValues) {
+        this.transitionTableValues = transitionTableValues;
+    }
+
+    /**
+     * Checks whether a DFA file line is valid
+     * @param DFAFileLine
+     * @return
+     */
+    public static boolean isDFAFileContentValid(String DFAFileLine) {
+        //TODO check if line is valid
+        return true;
+    }
+
+    /* GETTER METHODS */
+    
+    public ArrayList<TransitionTableElement> getTransitionTableValues() {
+        return transitionTableValues;
+    }
+
+    /* END GETTER METHODS */
 }
