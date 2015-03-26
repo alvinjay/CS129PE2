40f458c295715e511397d014ca2ca1560f8fde9e
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-03-26 13:54:41 +0800

    added Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+    Programmers: Alvin Jay Cosare, Gabriel Lagmay, Raphael Tugasan
+    Exercise 2: Strings and DFA, Date Due: March 31, 2015
+*/
+
+//import INTERFACE.GUI;
+
+/**
+ * Description:
+ *  -Main runnable class
+ */
+public class Main {
+    public static void main(String[] args) {
+//        new GUI();
+    }
+}
