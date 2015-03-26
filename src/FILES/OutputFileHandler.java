720107f7278d9afcc6a41e0fbb0cd48805f9b575
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-03-26 19:54:07 +0800

    added OutputFileHandler attributes and methods

--- src/FILES/OutputFileHandler.java
+++ src/FILES/OutputFileHandler.java
@@ -1,7 +1,22 @@
 package FILES;
 
+import java.util.ArrayList;
+
 /**
  * Created by alvinjay on 3/26/15.
  */
 public class OutputFileHandler {
+    private ArrayList<String> outputValues;
+
+    public OutputFileHandler(ArrayList<String> outputValues) {
+        this.outputValues = outputValues;
+    }
+
+    /* GETTER METHODS */
+
+    public ArrayList<String> getOutputValues() {
+        return outputValues;
+    }
+
+    /* END GETTER METHODS */
 }
